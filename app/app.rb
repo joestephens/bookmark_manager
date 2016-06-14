9055327547f1dbc981d08bff352441c6160fdf8b
Andy <andy.bell.github@gmail.com>
2016-06-14 14:59:02 +0100

    added second test and partial solution

--- app/app.rb
+++ app/app.rb
@@ -7,9 +7,13 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/links' do
-    erb :links
+    @links = Link.all
+    erb :'links/index'
   end
 
+  get '/links/new' do
+    erb :'links/new'
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
